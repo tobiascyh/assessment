87363a9c5db1bc3f470f886b97200749ce03dbab
Yee Hong Chu <tobiascyh@users.noreply.github.com>
2022-09-18 01:13:50 -0400

    Completed mandatory

--- Assessment.java
+++ Assessment.java
@@ -3,8 +3,11 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Assessment {
     public static void main(String[] args) {
@@ -15,48 +18,75 @@ public class Assessment {
         // Initialization
         String input = "";
         String line = "";
-        int n = 0;
+        int n = -1;
+        URL url = null;
         Scanner scan = new Scanner(System.in);
+        HashMap<String, Integer> storage = new HashMap<String, Integer>();
 
-        try {
-            System.out.println("Enter a  wikipedia link:");
-            input = scan.nextLine();
-            // Validating user provided url
-            URL url = new URL(input);
-            if (!url.getHost().contentEquals(wikiURL)) {
-                System.out.println("This is not a Wikipedia link");
-                System.exit(0);
+        // Program startup
+        while (n != 0) {
+            try {
+                System.out.println("Enter a Wikipedia URL:");
+                input = scan.nextLine().trim();
+                // Validating user provided url
+                url = new URL(input);
+                if (!url.getHost().toLowerCase().contentEquals(wikiURL)) {
+                    System.out.println("This is not a Wikipedia URL");
+                    break;
+                }
+            } catch (MalformedURLException e) {
+                System.out.println("URL entered is not valid: " + e.getMessage());
+                break;
             }
-        } catch (MalformedURLException e) {
-            System.out.println("URL entered is not valid: " + e.getMessage());
-            System.exit(0);
-        }
 
-        try {
-            URL url = new URL(input);
-            InputStream is = url.openStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
+            // Prompt n for initial startup only
+            if (n == -1) {
+                try {
+                    System.out.println("Run program for number of times (1 to 20):");
+                    n = scan.nextInt();
+                    scan.nextLine();
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid integer");
+                    break;
+                }
+                // Validating n range
+                if (n < 1 || n > 20) {
+                    System.out.println("Out of range");
+                    break;
+                }
             }
-            br.close();
-            is.close();
-        } catch (Exception e) {
-            System.out.println("Exception: " + e);
-            System.exit(0);
-        }
 
-        try {
-            System.out.println("Enter an integer between 1 to 20:");
-            n = scan.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println("Invalid integer");
-            System.exit(0);
+            // Scrape all links after "/wiki/" path
+            try {
+                InputStream is = url.openStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                while ((line = br.readLine()) != null) {
+                    Pattern p = Pattern.compile("a href=\"/wiki/(.*?)\"");
+                    Matcher m = p.matcher(line);
+                    String path = "";
+                    while (m.find()) {
+                        path = m.group(1);
+                        String s = url.getProtocol() + "://" + url.getHost() + "/wiki/" + path;
+                        if (storage.get(s) == null) {
+                            storage.put(s, 1);
+                        } else {
+                            storage.put(s, storage.get(s) + 1);
+                        }
+                    }
+                }
+                br.close();
+                is.close();
+            } catch (Exception e) {
+                System.out.println("Exception: " + e);
+                break;
+            }
+            n--;
         }
-        // Validating n range
-        if (n < 1 || n > 20) {
-            System.out.println("Out of range");
-            System.exit(0);
-        }
+        scan.close();
+        // Uncomment to view stored values in HashMap
+        //for (String s : storage.keySet()) {
+        //    System.out.println(s + " = " + storage.get(s));
+        //}
+        System.out.println("\nDone");
     }
 }
